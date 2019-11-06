0a3974ef9a2efb62e464db5a139f486994523cdd
luojunjie <luojunjie@inspur.com>
2019-11-06 22:03:06 +0800

    适配器模式基本类结构

--- src/adapter/base/Adapter.java
+++ src/adapter/base/Adapter.java
@@ -1,7 +1,7 @@
 package adapter.base;
 
 /**
- * 适配器，相当于是客户的代理人（这一点很像命令模式的Invoker）
+ * 适配器，相当于是客户的代理人（这一点很像命令模式的Invoker），但是实际进行的操作却像命令模式中的command对象（调用实际的方法）
  */
 public class Adapter implements Target {
     //持有被适配对象（被迫营业的对象）的抽象
